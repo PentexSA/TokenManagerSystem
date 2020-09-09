e2293403e6eecc97d6f57e664505a95b5b63675c
Assisneto Damasceno <netodamasceno12@gmail.com>
2020-09-08 21:42:46 -0300

    chore: add new colors

--- src/theme/appColors.ts
+++ src/theme/appColors.ts
@@ -4,6 +4,9 @@ const Colors = {
   primary: colors.EerieBlack,
   greenNero: colors.Nero,
   primaryText: colors.QuickSilver,
+  secondaryText: colors.Boulder,
+  primaryDotColor: colors.Alizarin,
+  secondaryDotColor: colors.Ivory,
 };
 
 export default Colors;
